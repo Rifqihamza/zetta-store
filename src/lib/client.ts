fbf83a8f16876762cb608a2c7010d5e19e6b91b6
Coders <rifqihamza30@gmail.com>
2026-01-20 18:40:34 +0700

    Update Production Client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,5 +4,6 @@ export const sanityClient = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
     apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-    useCdn: true,
+    useCdn: process.env.NODE_ENV === "production",
+    token: process.env.SANITY_READ_TOKEN,
 })
